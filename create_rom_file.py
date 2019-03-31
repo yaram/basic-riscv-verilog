918e4b7db4f67986f24c77369bd1eb12984161d0
Yaram <pootisspencerherenao@gmail.com>
2019-03-31 22:58:51 +1100

    Fix rom paths in create_rom_file.py

--- create_rom_file.py
+++ create_rom_file.py
@@ -1,6 +1,6 @@
-with open('rom.bin', 'rb') as bin_file:
+with open('build/rom.bin', 'rb') as bin_file:
     rom_bytes = bin_file.read()
 
-    with open('rom.hex', 'w') as hex_file:
+    with open('build/rom.hex', 'w') as hex_file:
         for byte in rom_bytes:
             hex_file.write('%0.2X ' % byte)
