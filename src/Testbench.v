0081d5055d0c4e3fe580c0e55d545b6d0507e025
Yaram <pootisspencerherenao@gmail.com>
2019-09-04 18:28:04 +1000

    Improve testbench

--- src/Testbench.v
+++ src/Testbench.v
@@ -10,7 +10,7 @@ module Testbench;
     integer i;
 
     initial begin
-        for(i = 0; i < ram_size; i++) begin
+        for(i = 0; i < ram_size; i = i + 1) begin
             ram[i] = 0;
         end
 
@@ -30,46 +30,44 @@ module Testbench;
     CPU cpu(clock, memory_address, memory_data_in, memory_data_out, memory_data_size, memory_enable, memory_operation, memory_ready);
 
     always @(posedge(memory_enable)) begin
-        if (memory_enable == 1) begin
-            if (memory_operation == 0) begin
-                if (memory_data_size == 0) begin
-                    $display("Memory Read: address %x, value %x", memory_address, ram[memory_address]);
-                end else if (memory_data_size == 1) begin
-                    $display("Memory Read: address %x, value %x", memory_address, {ram[memory_address + 1], ram[memory_address]});
-                end else if (memory_data_size == 2) begin
-                    $display("Memory Read: address %x, value %x", memory_address, {ram[memory_address + 3], ram[memory_address + 2], ram[memory_address + 1], ram[memory_address]});
-                end
-
-                memory_data_in[7 : 0] = ram[memory_address];
-                if (memory_data_size >= 1) memory_data_in[15 : 8] = ram[memory_address + 1];
-                if (memory_data_size >= 2) begin
-                    memory_data_in[23 : 16] = ram[memory_address + 2];
-                    memory_data_in[31 : 24] = ram[memory_address + 3];
-                end
-
-                memory_ready = 1;
-                @(negedge memory_enable);
-                memory_ready = 0;
-            end else begin
-                if (memory_data_size == 0) begin
-                    $display("Memory Write: address %x, old value %x, new value %x", memory_address, ram[memory_address], memory_data_out[7 : 0]);
-                end else if (memory_data_size == 1) begin
-                    $display("Memory Write: address %x, old value %x, new value %x", memory_address, {ram[memory_address + 1], ram[memory_address]}, memory_data_out[15 : 0]);
-                end else if (memory_data_size == 2) begin
-                    $display("Memory Write: address %x, old value %x, new value %x", memory_address, {ram[memory_address + 3], ram[memory_address + 2], ram[memory_address + 1], ram[memory_address]}, memory_data_out);
-                end
-
-                ram[memory_address] = memory_data_out[7 : 0];
-                if (memory_data_size >= 1) ram[memory_address + 1] = memory_data_out[15 : 8];
-                if (memory_data_size >= 2) begin
-                    ram[memory_address + 2] = memory_data_out[23 : 16];
-                    ram[memory_address + 3] = memory_data_out[31 : 24];
-                end
-
-                memory_ready = 1;
-                @(negedge memory_enable);
-                memory_ready = 0;
+        if (memory_operation == 0) begin
+            if (memory_data_size == 0) begin
+                $display("Memory Read: address %x, value %x", memory_address, ram[memory_address]);
+            end else if (memory_data_size == 1) begin
+                $display("Memory Read: address %x, value %x", memory_address, {ram[memory_address + 1], ram[memory_address]});
+            end else if (memory_data_size == 2) begin
+                $display("Memory Read: address %x, value %x", memory_address, {ram[memory_address + 3], ram[memory_address + 2], ram[memory_address + 1], ram[memory_address]});
             end
+
+            memory_data_in[7 : 0] = ram[memory_address];
+            if (memory_data_size >= 1) memory_data_in[15 : 8] = ram[memory_address + 1];
+            if (memory_data_size >= 2) begin
+                memory_data_in[23 : 16] = ram[memory_address + 2];
+                memory_data_in[31 : 24] = ram[memory_address + 3];
+            end
+
+            memory_ready = 1;
+            @(negedge memory_enable);
+            memory_ready = 0;
+        end else begin
+            if (memory_data_size == 0) begin
+                $display("Memory Write: address %x, old value %x, new value %x", memory_address, ram[memory_address], memory_data_out[7 : 0]);
+            end else if (memory_data_size == 1) begin
+                $display("Memory Write: address %x, old value %x, new value %x", memory_address, {ram[memory_address + 1], ram[memory_address]}, memory_data_out[15 : 0]);
+            end else if (memory_data_size == 2) begin
+                $display("Memory Write: address %x, old value %x, new value %x", memory_address, {ram[memory_address + 3], ram[memory_address + 2], ram[memory_address + 1], ram[memory_address]}, memory_data_out);
+            end
+
+            ram[memory_address] = memory_data_out[7 : 0];
+            if (memory_data_size >= 1) ram[memory_address + 1] = memory_data_out[15 : 8];
+            if (memory_data_size >= 2) begin
+                ram[memory_address + 2] = memory_data_out[23 : 16];
+                ram[memory_address + 3] = memory_data_out[31 : 24];
+            end
+
+            memory_ready = 1;
+            @(negedge memory_enable);
+            memory_ready = 0;
         end
     end
 
