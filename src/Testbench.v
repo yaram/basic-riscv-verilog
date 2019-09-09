aa1b6826511ad83fd87f1bbf617be2338a2de087
Yaram <pootisspencerherenao@gmail.com>
2019-09-09 17:59:22 +1000

    Handle memory access out of ram in testbench

--- src/Testbench.v
+++ src/Testbench.v
@@ -33,38 +33,44 @@ module Testbench;
 
     always @(posedge(memory_enable)) begin
         if (memory_operation == 0) begin
-            if (memory_data_size == 0) begin
-                $display("Memory Read: address %x, value %x", memory_address, ram[memory_address]);
-            end else if (memory_data_size == 1) begin
-                $display("Memory Read: address %x, value %x", memory_address, {ram[memory_address + 1], ram[memory_address]});
-            end else if (memory_data_size == 2) begin
-                $display("Memory Read: address %x, value %x", memory_address, {ram[memory_address + 3], ram[memory_address + 2], ram[memory_address + 1], ram[memory_address]});
-            end
+            if (memory_address < ram_size) begin
+                if (memory_data_size == 0) begin
+                    $display("Memory Read: address %x, value %x", memory_address, ram[memory_address]);
+                end else if (memory_data_size == 1) begin
+                    $display("Memory Read: address %x, value %x", memory_address, {ram[memory_address + 1], ram[memory_address]});
+                end else if (memory_data_size == 2) begin
+                    $display("Memory Read: address %x, value %x", memory_address, {ram[memory_address + 3], ram[memory_address + 2], ram[memory_address + 1], ram[memory_address]});
+                end
 
-            memory_data_in[7 : 0] = ram[memory_address];
-            if (memory_data_size >= 1) memory_data_in[15 : 8] = ram[memory_address + 1];
-            if (memory_data_size >= 2) begin
-                memory_data_in[23 : 16] = ram[memory_address + 2];
-                memory_data_in[31 : 24] = ram[memory_address + 3];
+                memory_data_in[7 : 0] = ram[memory_address];
+                if (memory_data_size >= 1) memory_data_in[15 : 8] = ram[memory_address + 1];
+                if (memory_data_size >= 2) begin
+                    memory_data_in[23 : 16] = ram[memory_address + 2];
+                    memory_data_in[31 : 24] = ram[memory_address + 3];
+                end
+            end else begin
+                memory_data_in = 0;
             end
 
             memory_ready = 1;
             @(negedge memory_enable);
             memory_ready = 0;
         end else begin
-            if (memory_data_size == 0) begin
-                $display("Memory Write: address %x, old value %x, new value %x", memory_address, ram[memory_address], memory_data_out[7 : 0]);
-            end else if (memory_data_size == 1) begin
-                $display("Memory Write: address %x, old value %x, new value %x", memory_address, {ram[memory_address + 1], ram[memory_address]}, memory_data_out[15 : 0]);
-            end else if (memory_data_size == 2) begin
-                $display("Memory Write: address %x, old value %x, new value %x", memory_address, {ram[memory_address + 3], ram[memory_address + 2], ram[memory_address + 1], ram[memory_address]}, memory_data_out);
-            end
+            if (memory_address < ram_size) begin
+                if (memory_data_size == 0) begin
+                    $display("Memory Write: address %x, old value %x, new value %x", memory_address, ram[memory_address], memory_data_out[7 : 0]);
+                end else if (memory_data_size == 1) begin
+                    $display("Memory Write: address %x, old value %x, new value %x", memory_address, {ram[memory_address + 1], ram[memory_address]}, memory_data_out[15 : 0]);
+                end else if (memory_data_size == 2) begin
+                    $display("Memory Write: address %x, old value %x, new value %x", memory_address, {ram[memory_address + 3], ram[memory_address + 2], ram[memory_address + 1], ram[memory_address]}, memory_data_out);
+                end
 
-            ram[memory_address] = memory_data_out[7 : 0];
-            if (memory_data_size >= 1) ram[memory_address + 1] = memory_data_out[15 : 8];
-            if (memory_data_size >= 2) begin
-                ram[memory_address + 2] = memory_data_out[23 : 16];
-                ram[memory_address + 3] = memory_data_out[31 : 24];
+                ram[memory_address] = memory_data_out[7 : 0];
+                if (memory_data_size >= 1) ram[memory_address + 1] = memory_data_out[15 : 8];
+                if (memory_data_size >= 2) begin
+                    ram[memory_address + 2] = memory_data_out[23 : 16];
+                    ram[memory_address + 3] = memory_data_out[31 : 24];
+                end
             end
 
             memory_ready = 1;
