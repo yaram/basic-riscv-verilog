1b5fff1a20d27eaf196fa82eabaff2438a1c2f28
Yaram <pootisspencerherenao@gmail.com>
2019-10-20 20:29:01 +1100

    Build ROM ELF binary as well as flat binary

--- build_rom.py
+++ build_rom.py
@@ -19,7 +19,8 @@ if not os.path.exists(build_directory):
 run_command('clang', '-target', 'riscv32-unknown-unknown-elf', '-march=rv32i', '-c', '-o', os.path.join(build_directory, 'asm.o'), os.path.join(source_directory, 'asm.S'))
 run_command('clang', '-target', 'riscv32-unknown-unknown-elf', '-march=rv32i', '-c', '-o', os.path.join(build_directory, 'main.o'), os.path.join(source_directory, 'main.c'))
 
-run_command('ld.lld', '-T', os.path.join(source_directory, 'linker.ld'), '-o', os.path.join(build_directory, 'rom.bin'), '-e', 'entry', '-m', 'elf32lriscv', '--oformat', 'binary', os.path.join(build_directory, 'asm.o'), os.path.join(build_directory, 'main.o'))
+run_command('ld.lld', '-T', os.path.join(source_directory, 'linker.ld'), '-o', os.path.join(build_directory, 'rom.elf'), '-e', 'entry', '-m', 'elf32lriscv', os.path.join(build_directory, 'asm.o'), os.path.join(build_directory, 'main.o'))
+run_command('llvm-objcopy', '--output-target=binary', os.path.join(build_directory, 'rom.elf'), os.path.join(build_directory, 'rom.bin'))
 
 with open(os.path.join(build_directory, 'rom.bin'), 'rb') as bin_file:
     rom_bytes = bin_file.read()
